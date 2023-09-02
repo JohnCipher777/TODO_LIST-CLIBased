f71a563f722cc0a1a22a0c7d5b0ae13c4f4c421a
ᴊᴏʜɴ ᴅ' ᴄɪᴘʜᴇʀ <123635909+JohnCipher777@users.noreply.github.com>
2023-09-02 21:57:52 +0800

    Update TODO-LIST_CLI.c

--- TODO_LIST-CLI/TODO-LIST_CLI.c
+++ TODO_LIST-CLI/TODO-LIST_CLI.c
@@ -51,34 +51,34 @@ int main(){
   	}while(notfound);
 }
 char *printLicense(){
-	char *str = (char *)malloc(sizeof(char) * MAX_LENGTH); // Allocate memory for a string
+    char *str = (char *)malloc(sizeof(char) * MAX_LENGTH); // Allocate memory for a string
     bool notfound = true;
 
     do{
     	system("cls");
     	printf("Custom CLI Software License\n\n");
     	printf("Copyright (c) 2023 John D' Cipher\n");
-	    printf("Version 1.0, [09/2/2023]\n\n");
-	    printf("Permission is hereby granted, free of charge, to any person obtaining a copy of this\n");
-		printf("software and associated documentation files, to use the Software for personal, educational,\n");
-		printf("or non-commercial purposes, subject to the following conditions:\n");
-		printf("\n");
-	    printf("You may freely use, modify, and distribute the Software for personal, educational, or\n");
-		printf("non-commercial purposes. However, commercial distribution of the Software, which includes\n");
-		printf("selling, licensing, or bundling the Software for profit, is strictly prohibited without the\n");
-		printf("explicit written approval of the software author, John Cipher. Any redistributions of the Software\n");
-		printf("must retain this license notice and all copyright notices contained in the Software.\n");
-	    printf("\n");
-	    printf("The Software is provided \"as is,\" without warranty of any kind, express or implied, including\n");
-		printf("but not limited to the warranties of merchantability, fitness for a particular purpose, and\n");
-		printf("non-infringement. In no event shall the author or copyright holders be liable for any claim,\n");
-		printf("damages, or other liability, whether in an action of contract, tort, or otherwise, arising from,\n");
-		printf("out of, or in connection with the Software or the use or other dealings in the Software.\n");
-		printf("\n");
-	    printf("By using, modifying, or distributing the Software, you agree to abide by the terms and conditions of this license.\n\n");
-	    printf("John Cipher\n");
-	    printf("https://github.com/JohnCipher777\n");
-	    printf("______________________________________________________________________\n");
+        printf("Version 1.0, [09/2/2023]\n\n");
+        printf("Permission is hereby granted, free of charge, to any person obtaining a copy of this\n");
+        printf("software and associated documentation files, to use the Software for personal, educational,\n");
+	printf("or non-commercial purposes, subject to the following conditions:\n");
+	printf("\n");
+	printf("You may freely use, modify, and distribute the Software for personal, educational, or\n");
+	printf("non-commercial purposes. However, commercial distribution of the Software, which includes\n");
+	printf("selling, licensing, or bundling the Software for profit, is strictly prohibited without the\n");
+	printf("explicit written approval of the software author, John Cipher. Any redistributions of the Software\n");
+	printf("must retain this license notice and all copyright notices contained in the Software.\n");
+	printf("\n");
+	printf("The Software is provided \"as is,\" without warranty of any kind, express or implied, including\n");
+	printf("but not limited to the warranties of merchantability, fitness for a particular purpose, and\n");
+	printf("non-infringement. In no event shall the author or copyright holders be liable for any claim,\n");
+	printf("damages, or other liability, whether in an action of contract, tort, or otherwise, arising from,\n");
+	printf("out of, or in connection with the Software or the use or other dealings in the Software.\n");
+	printf("\n");
+	printf("By using, modifying, or distributing the Software, you agree to abide by the terms and conditions of this license.\n\n");
+	printf("John Cipher\n");
+	printf("https://github.com/JohnCipher777\n");
+	printf("______________________________________________________________________\n");
         // Ask for user agreement
         printf("\nDo you agree to the terms & conditions? (yes/no): ");
         
@@ -112,6 +112,5 @@ char *printLicense(){
             printf("press enter key to try again...");
 	        getchar();
         }
-        
     }while (strlen(str)>MAX_LENGTH || notfound);
 }
